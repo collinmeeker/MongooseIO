c08ae18c928954a67b91180e6aebf5c7ee24a329
Collin Meeker <119772537+collinmeeker@users.noreply.github.com>
2024-04-20 18:27:13 -0400

    Add files via upload

---
+++ parallel_id.h
@@ -0,0 +1,18 @@
+
+#ifndef EMPLOYEE_GENERATOR_H
+#define EMPLOYEE_GENERATOR_H
+
+#include <pthread.h>
+
+
+
+void setThreads(int threads);
+
+// Function executed by each thread to generate a random 9-digit employee ID
+void *generate_employee_id(void *arg);
+
+// Function to generate employee IDs using multiple threads
+int generate_employee_ids_multithreaded();
+
+#endif // EMPLOYEE_GENERATOR_H
+
